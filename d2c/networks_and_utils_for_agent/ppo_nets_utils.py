7d425afb8a1d0c4e266eb995dc7a718de9a53693
XJY <464672479@qq.com>
2025-12-15 16:09:03 +0800

    update ppo nets

--- d2c/networks_and_utils_for_agent/ppo_nets_utils.py
+++ d2c/networks_and_utils_for_agent/ppo_nets_utils.py
@@ -1,17 +1,26 @@
-import gymnasium as gym
+import gym
 import numpy as np
 import torch
 import torch.nn as nn
 from torch.distributions.categorical import Categorical
+from torch.distributions.normal import Normal
+from typing import Tuple, List, Union, Type, Optional, Sequence
+from gym.spaces import Box, Space
 
-def make_env(env_id, idx, capture_video, run_name):
+def make_env(env_id, idx, capture_video, run_name, gamma):
     def thunk():
         if capture_video and idx == 0:
             env = gym.make(env_id, render_mode="rgb_array")
             env = gym.wrappers.RecordVideo(env, f"videos/{run_name}")
         else:
             env = gym.make(env_id)
+        env = gym.wrappers.FlattenObservation(env)  # deal with dm_control's Dict observation space
         env = gym.wrappers.RecordEpisodeStatistics(env)
+        env = gym.wrappers.ClipAction(env)
+        env = gym.wrappers.NormalizeObservation(env)
+        env = gym.wrappers.TransformObservation(env, lambda obs: np.clip(obs, -10, 10))
+        env = gym.wrappers.NormalizeReward(env, gamma=gamma)
+        env = gym.wrappers.TransformReward(env, lambda reward: np.clip(reward, -10, 10))
         return env
 
     return thunk
@@ -33,20 +42,77 @@ class Agent(nn.Module):
             nn.Tanh(),
             layer_init(nn.Linear(64, 1), std=1.0),
         )
-        self.actor = nn.Sequential(
+        self.actor_mean = nn.Sequential(
             layer_init(nn.Linear(np.array(envs.single_observation_space.shape).prod(), 64)),
             nn.Tanh(),
             layer_init(nn.Linear(64, 64)),
             nn.Tanh(),
-            layer_init(nn.Linear(64, envs.single_action_space.n), std=0.01),
+            layer_init(nn.Linear(64, np.prod(envs.single_action_space.shape)), std=0.01),
         )
+        self.actor_logstd = nn.Parameter(torch.zeros(1, np.prod(envs.single_action_space.shape)))
 
     def get_value(self, x):
         return self.critic(x)
 
     def get_action_and_value(self, x, action=None):
-        logits = self.actor(x)
-        probs = Categorical(logits=logits)
+        action_mean = self.actor_mean(x)
+        action_logstd = self.actor_logstd.expand_as(action_mean)
+        action_std = torch.exp(action_logstd)
+        probs = Normal(action_mean, action_std)
         if action is None:
             action = probs.sample()
-        return action, probs.log_prob(action), probs.entropy(), self.critic(x)
+        return action, probs.log_prob(action).sum(1), probs.entropy().sum(1), self.critic(x)
+
+
+class ActorNetwork(nn.Module):
+    def __init__(
+            self,
+            observation_space: Union[Box, Space],
+            action_space: Union[Box, Space],
+            fc_layers_params: Sequence[int] = (),
+            device: Union[str, int, torch.device] = 'cpu',
+    ):
+        super().__init__()
+        self.observation_dim = observation_space.shape[0]
+        self.action_dim = action_space.shape[0]
+        self.arch = "-".join(map(str, fc_layers_params))
+        self.device = device
+        self.actor_mean = nn.Sequential(
+            layer_init(nn.Linear(np.array(self.observation_dim).prod(),64)),
+            nn.Tanh(),
+            layer_init(nn.Linear(64,64)),
+            nn.Tanh(),
+            layer_init(nn.Linear(64, np.prod(self.action_dim)), std=0.01)
+        )
+        self.actor_logstd = nn.Parameter(torch.zeros(1, np.prod(self.action_dim)))
+    
+    def forward(self, x, action=None):
+        action_mean = self.actor_mean(x)
+        action_logstd = self.actor_logstd.expand_as(action_mean)
+        action_std = torch.exp(action_logstd)
+        probs = Normal(action_mean, action_std)
+        if action is None:
+            action = probs.sample()
+        return action, probs.log_prob(action).sum(1), probs.entropy().sum(1)
+    
+class CriticNetwork(nn.Module):
+    def __init__(
+            self,
+            observation_space: Union[Box, Space],
+            fc_layers_params: Sequence[int] = (),
+            device: Union[str, int, torch.device] = 'cpu',
+    ):
+        super().__init__()
+        self.observation_dim = observation_space.shape[0]
+        self.arch = "-".join(map(str, fc_layers_params))
+        self.device = device
+        self.critic = nn.Sequential(
+            layer_init(nn.Linear(np.array(self.observation_dim).prod(),64)),
+            nn.Tanh(),
+            layer_init(nn.Linear(64,64)),
+            nn.Tanh(),
+            layer_init(nn.Linear(64, 1), std=1.0),
+        )
+    
+    def forward(self, x):
+        return self.critic(x)
