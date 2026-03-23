ee4e9dd4ab032af439996336e31aa810c6236fb5
XJY <464672479@qq.com>
2026-03-23 18:57:59 +0800

    delete useless algo

--- d2c/models/__init__.py
+++ d2c/models/__init__.py
@@ -5,27 +5,15 @@ from d2c.utils import utils
 from d2c.utils.replaybuffer import ReplayBuffer
 from d2c.models.base import BaseAgent
 from d2c.models.model_based.mbpo import MBPOAgent
-from d2c.models.model_free.td3_bc import TD3BCAgent
-from d2c.models.model_free.cql import CQLAgent
-from d2c.models.model_free.doge import DOGEAgent
 from d2c.models.model_free.h2o import H2OAgent
 from d2c.models.model_free.darc import DARCAgent
-from d2c.models.imitation.dmil import DMILAgent
-from d2c.models.imitation.bc import BCAgent
-from d2c.models.model_free.iql import IQLAgent
 from d2c.models.model_free.sac import SACAgent
 from d2c.models.model_free.ppo import PPOAgent
 
 AGENT_MODULES_DICT = {
     'mbpo': MBPOAgent,
-    'td3_bc': TD3BCAgent,
-    'cql': CQLAgent,
-    'doge': DOGEAgent,
     'h2o': H2OAgent,
     'darc': DARCAgent,  
-    'dmil': DMILAgent,
-    'bc': BCAgent,
-    'iql': IQLAgent,
     'sac': SACAgent,
     'ppo': PPOAgent,
 }
